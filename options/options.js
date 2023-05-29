a67cb490b025b531d3bbc58db58bd7982b3257a5
Muhammad Talha <muhammad.talha@crowdbotics.com>
2023-05-29 18:28:10 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "test-app-41070",
-  url: "https://test-app-41070.botics.co",
-  api: "https://test-app-41070.botics.co/api/v1"
-}
+  "name": "test-app-41070",
+  "url": "https://test-app-41070.botics.co",
+  "api": "https://test-app-41070.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
